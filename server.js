ac9d4cae9b0d1cf2ce5447fd29e2e9a54d93c4c2
William Norman <lordcrekit@gmail.com>
2017-05-01 16:59:31 -0400

    linting

--- server.js
+++ server.js
@@ -9,46 +9,46 @@ DATA[makeid()] = { task: 'test id randomness', done: false }
 
 // homepage
 app.get('/', function (req, res, next) {
-    res.send('Welcome! Go to /todos for the api')
+  res.send('Welcome! Go to /todos for the api')
 })
 
-// get all todos 
+// get all todos
 app.get('/todos', function (req, res, next) {
   res.json(DATA)
 })
 
 // get one todo
 app.get('/todos/:id', function (req, res, next) {
-    let found = DATA[req.params.id]
-    if (!found) { res.status(404).end(req.params.id + ' not found'); return }
-    res.json(found)
+  let found = DATA[req.params.id]
+  if (!found) { res.status(404).end(req.params.id + ' not found'); return }
+  res.json(found)
 })
 
 // add new todo
 app.post('/todos', function (req, res, next) {
-    if (!req.body.task) { res.status(400).end('must enter task!'); return }
-    DATA[makeid()] = {
-        "task": req.body.task,
-        "done": req.body.done || false
-    }
-    res.redirect('/todos')
+  if (!req.body.task) { res.status(400).end('must enter task!'); return }
+  DATA[makeid()] = {
+    'task': req.body.task,
+    'done': req.body.done || false
+  }
+  res.redirect('/todos')
 })
 
 // update todo
 app.put('/todos/:id', function (req, res, next) {
-    let found = DATA[req.params.id]
-    if (!found) { res.status(404).end(req.params.id + ' not found'); return }
-    if (req.body.task) { found.task = req.body.task }
-    if (undefined !== req.body.done) { found.done = req.body.done }
-    res.redirect('/todos')
+  let found = DATA[req.params.id]
+  if (!found) { res.status(404).end(req.params.id + ' not found'); return }
+  if (req.body.task) { found.task = req.body.task }
+  if (undefined !== req.body.done) { found.done = req.body.done }
+  res.redirect('/todos')
 })
 
 // delete todo
 app.delete('/todos/:id', function (req, res, next) {
-    let found = DATA[req.params.id]
-    if (!found) { res.status(404).end(req.params.id + ' not found'); return }
-    delete DATA[req.params.id]
-    res.redirect('/todos')
+  let found = DATA[req.params.id]
+  if (!found) { res.status(404).end(req.params.id + ' not found'); return }
+  delete DATA[req.params.id]
+  res.redirect('/todos')
 })
 
 // Handle 404
